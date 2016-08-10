e22b96d23217802b6b493b9c6d3a42182d897347
MindMC <themindofminecraft@gmail.com>
2016-08-10 15:07:40 -0400

    Revert "Initial Commit"
    This reverts commit 5aa6c70f333eb4598d10aa4f31ad15422268ce05.

--- src/main/java/methods/ConfigReader.java
+++
@@ -1,8 +0,0 @@
-package methods;
-
-import java.io.File;
-import java.util.TreeMap;
-
-public interface ConfigReader {
-	 TreeMap<String, Config> readConfig(); //Abstract readConfig method to be overriden by ConfigHandler
-}
--- src/main/java/methods/Reference.java
+++
@@ -1,8 +0,0 @@
-package methods;
-
-public class Reference { //Class that contains variables with mod information
-	
-	public static final String MOD_ID = "ReplyMod";
-	public static final String MOD_NAME = "Auto Reply Mod";
-	public static final String VERSION = "1.1.7";
-}
